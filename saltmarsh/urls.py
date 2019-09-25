58c6563719385c81f89fef658befa21f3921298d
Andy Hunt <andy.l.hunt85@gmail.com>
2019-09-25 08:15:54 -0400

    Added article creation, list, deletion, updated styles throughout the site

--- saltmarsh/urls.py
+++ saltmarsh/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
 from . import views
+from .views import ArticleListView, ArticleDetailView, ArticleCreateView, ArticleDeleteView
 
 urlpatterns = [
-    path('', views.home, name="home"),
+    path('', ArticleListView.as_view(), name="home"),
+    path('article/<int:pk>/', ArticleDetailView.as_view(), name='article-detail'),
+    path('article/new/', ArticleCreateView.as_view(), name='article-create'),
+    path('article/<int:pk>/delete/', ArticleDeleteView.as_view(), name='article-delete'),
     path('about/', views.about, name="about"),
 ]
--- saltmarsh/views.py
+++ saltmarsh/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
-from .models import Sessions
+from .models import Sessions, Article
+from django.views.generic import ListView, DetailView, CreateView, DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 
 def home(request):
     context = {
@@ -9,3 +11,28 @@ def home(request):
 
 def about(request):
     return render(request, "saltmarsh/about.html", { 'title': 'About'})
+
+class ArticleListView(ListView):
+    model = Article
+    template_name = "saltmarsh/home.html"
+    context_object_name = "articles"
+    ordering = ['-date_posted']
+
+class ArticleDetailView(DetailView):
+    model = Article
+
+class ArticleCreateView(LoginRequiredMixin, CreateView):
+    model = Article
+    fields = ['title', 'content']
+    def form_valid(self, form):
+        form.instance.author = self.request.user 
+        return super().form_valid(form)
+
+class ArticleDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Article 
+    success_url = "/"
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True 
+        return False
