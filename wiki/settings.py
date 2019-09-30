927cbd7766671893b89c2bcac0f10cc849953120
Andy Hunt <andy.l.hunt85@gmail.com>
2019-09-30 17:04:19 -0400

    Updated environment variables and Settings

--- wiki/settings.py
+++ wiki/settings.py
@@ -20,12 +20,13 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '#l%_-505pxm(!b0%*gly@h39w@#7!cfnv)o9gy=-3z&moh-5%k'
+#SECRET_KEY = '#l%_-505pxm(!b0%*gly@h39w@#7!cfnv)o9gy=-3z&moh-5%k'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['saltmarsh-wiki.herokuapp.com']
 
 
 # Application definition
