acce726eff0083b217b20a2105ac83f7111cd799
Andy Hunt <andy.l.hunt85@gmail.com>
2019-10-02 20:03:34 -0400

    Updated debug

--- wiki/settings.py
+++ wiki/settings.py
@@ -25,9 +25,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-INTERNAL_IPS = (
-    '69.143.96.57'
-)
+
+DEBUG = (os.environ.get("DEBUG_VALUE") == "True")
 
 ALLOWED_HOSTS = ['saltmarsh-wiki.herokuapp.com']
 
