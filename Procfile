93d9f400233d64883c8c7d119dbd849534ce3738
Andy Hunt <andy.l.hunt85@gmail.com>
2019-10-02 20:44:51 -0400

    Updated procfile

--- Procfile
+++ Procfile
@@ -1,1 +1,2 @@
 web: gunicorn wiki.wsgi
+python manage.py collectstatic --noinput
--- wiki/settings.py
+++ wiki/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = (os.environ.get('DEBUG_VALUE') == 'True')
 
-ALLOWED_HOSTS = ['https://saltmarsh-wiki.herokuapp.com/']
+ALLOWED_HOSTS = ['https://saltmarsh-wiki.herokuapp.com']
 
 
 # Application definition
