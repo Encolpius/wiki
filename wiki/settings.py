5a49c2f332a01401ac0eb2d0080cc6a59127c6e3
Andy Hunt <andy.l.hunt85@gmail.com>
2019-10-07 20:24:26 -0400

    updated environ

--- wiki/settings.py
+++ wiki/settings.py
@@ -14,8 +14,9 @@ import os
 import django_heroku
 import environ
 env = environ.Env(
-    DEBUG = (bool, True)
+    DEBUG = (bool, False)
 )
+environ.Env.read_env()
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
