e73aa4649482e310ed3cf79b9d6526c69e6ca08d
Andy Hunt <andy.l.hunt85@gmail.com>
2019-10-08 20:25:44 -0400

    Added race/class/backgorund updates to profile

--- users/models.py
+++ users/models.py
@@ -5,12 +5,36 @@ from PIL import Image
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     image = models.ImageField(default="default.png", upload_to="profile_pics")
+    background = models.TextField(default="No background created yet.")
+
+    classes = (
+        ('Barbarian', 'BARBARIAN'),
+        ('Bard', 'BARD'),
+        ('Cleric', 'CLERIC'),
+        ('Druid', 'DRUID'),
+        ('Fighter', 'FIGHTER'),
+        ('Monk', 'MONK'),
+        ('Paladin', 'PALADIN'),
+        ('Ranger', 'RANGER'),
+        ('Rogue', 'ROGUE'),
+        ('Sorcerer', 'SORCERER'),
+        ('Warlock', 'WARLOCK'),
+        ('Wizard', 'WIZARD'),
+    )
+
+    races = (
+        ('Dragonborn', 'DRAGONBORN'),
+        ('Dwarf', 'DWARF'),
+        ('Elf', 'ELF'),
+        ('Gnome', 'GNOME'),
+        ('Half-elf', 'HALF-ELF'),
+        ('Halfling', 'HALFLING'),
+        ('Half-orc', 'HALF-ORC'),
+        ('Human', 'HUMAN'),
+        ('Tiefling', 'TIEFLING'),
+    )
+    charClass = models.CharField(max_length=10, choices=classes, default="")
+    race = models.CharField(max_length=15, choices=races, default="")
+    
     def __str__(self):
         return f'{self.user.username} Profile'
-    # def save(self, *args, **kwargs):
-    #   super().save(*args, **kwargs)
-    #   img = Image.open(self.image.path)
-    #   if img.height > 300 or img.width > 300:
-    #       output_size = (300, 300)
-    #       img.thumbnail(output_size)
-    #       img.save(self.image.path)
