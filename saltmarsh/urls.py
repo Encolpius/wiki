a5359f3dd26346374a348e11b98babf4c8f5b908
Andy Hunt <andy.l.hunt85@gmail.com>
2019-10-22 19:23:26 -0400

    Added styles to Recent Comments section

--- saltmarsh/urls.py
+++ saltmarsh/urls.py
@@ -10,9 +10,9 @@ from .views import (
 
 urlpatterns = [
     path('', ArticleListView.as_view(), name="home"),
+    path('article/new/', ArticleCreateView.as_view(), name='article-create'),
     path('article/<int:pk>/', ArticleDetailView.as_view(), name='article-detail'),
     path('article/<slug>/', ArticleDetailView.as_view(), name="article-detail"),
-    path('article/new/', ArticleCreateView.as_view(), name='article-create'),
     path('article/<int:pk>/update', ArticleUpdateView.as_view(), name='article-update'),
     path('article/<int:pk>/delete/', ArticleDeleteView.as_view(), name='article-delete'),
     path('article/<int:pk>/comment/', views.add_comment_to_article, name='add-comment-to-article'),
--- saltmarsh/views.py
+++ saltmarsh/views.py
@@ -21,7 +21,7 @@ class ArticleListView(ListView):
     ordering = ['-date_posted']
     def get_context_data(self, **kwargs):
         context = super(ArticleListView, self).get_context_data(**kwargs)
-        context['comments'] = Comment.objects.all()
+        context['comments'] = Comment.objects.all()[:5]
         return context
 
 class ArticleDetailView(DetailView):
