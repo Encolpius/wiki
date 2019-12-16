39199b73e35648b88b6eaa44cafed16491f00fcc
Andy Hunt <andy.l.hunt85@gmail.com>
2019-12-15 21:36:10 -0500

    updated tab functionality to JS, changed comment margins

--- saltmarsh/static/saltmarsh/script.js
+++ saltmarsh/static/saltmarsh/script.js
@@ -1,24 +1,36 @@
-function openNavTab(evt, tabName) {
-    let tablinks = document.getElementsByClassName("tablinks");
-    for (let i = 0; i < tablinks.length; i++) {
+let tabs = document.getElementsByClassName('tablinks');
+let tabcontent = document.getElementsByClassName("tabcontent");
+
+for (let i = 0; i < tabs.length; i++) {
+    tabs[i].addEventListener("mouseover", openNavTab);
+    tabs[i].addEventListener("mouseleave", closeNavTab);
+}
+
+for (let i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].addEventListener("mouseenter", keepTabOpen);
+    tabcontent[i].addEventListener("mouseleave", closeNavTab);
+}
+
+function openNavTab(tabName) {
+    let tab = tabName.srcElement.innerHTML;
+    for (let i = 0; i < tabs; i++) {
         tablinks[i].className = tablinks[i].className.replace(" active", "");
     }
-    document.getElementById(tabName).style.display = "block";
-    updateMargin(tabName);
+    document.getElementById(tab).style.display = "block";
+    updateMargin(tab);
 }
 
 function updateMargin(tabName) {
     let tab = document.getElementById(tabName);
-    tabName == "Players"      ?  tab.style.marginLeft = "8px" :
-        tabName == "Sessions" ?  tab.style.marginLeft = "90px" : 
-        tabName == "NPCs"     ?  tab.style.marginLeft = "185px": 
-        tabName == "Places"   ?  tab.style.marginLeft = "250px" : 
-        tabName == "Villains" ?  tab.style.marginLeft = "335px" : 
+    tabName == "Players"      ?  tab.style.marginLeft = "2px" :
+        tabName == "Sessions" ?  tab.style.marginLeft = "88px" : 
+        tabName == "NPCs"     ?  tab.style.marginLeft = "145px": 
+        tabName == "Places"   ?  tab.style.marginLeft = "242px" : 
+        tabName == "Villains" ?  tab.style.marginLeft = "330px" : 
         tabName;
 }
 
 function closeNavTab() {
-    let tabcontent = document.getElementsByClassName("tabcontent");
     for (i = 0; i < tabcontent.length; i++) {
         tabcontent[i].style.display = "none";
     }
--- saltmarsh/views.py
+++ saltmarsh/views.py
@@ -8,12 +8,6 @@ from django.contrib.auth.models import User
 
 recent_comments = Comment.objects.all().order_by("-date_posted")[:5]
 
-def home(request):
-    context = {
-        'sessions': Sessions.objects.all(),
-    }
-    return render(request, "saltmarsh/home.html", context)
-
 def about(request):
     context = {
         'title': 'About',
@@ -29,6 +23,7 @@ class ArticleListView(ListView):
     def get_context_data(self, **kwargs):
         context = super(ArticleListView, self).get_context_data(**kwargs)
         context['recent_comments'] = recent_comments
+        context['pages'] = Article.objects.count()
         return context
 
 class ArticleDetailView(DetailView):
